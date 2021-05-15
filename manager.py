2035c44fb783527532f79df1d371d56e6070f075
sardpade <sunwh1004184@163.com>
2021-05-15 13:15:07 +0800

    创建redis对象和csrf对象

--- manager.py
+++ manager.py
@@ -1,15 +1,29 @@
 from flask import Flask
 # 导入数据库扩展，并在配置中加载配置
 from flask_sqlalchemy import SQLAlchemy
-
-
+from redis import StrictRedis
+#CSRFProtect只做验证工作
+from flask_wtf.csrf import CSRFProtect
 
 class Config(object):
 
     DEBUG = True
 
-    SQLALCHEMY_DATABASE_URI = "mysql://root：mysql@127.0.0.1:3306/information"
-    SQLALCHEMY_TRACK_MODIFICATION = True
+    SQLALCHEMY_DATABASE_URI = "mysql://root：mysql@127.0.0.1:3306/new"
+    # 开启数据库跟踪
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # redis数据库配置信息
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+    # 使用第七个数据库
+    REDIS_NUM = 7
+
+
+
+
+
+
+
 
 # 1.创建app对象
 app = Flask(__name__)
@@ -17,10 +31,12 @@ app = Flask(__name__)
 app.config.from_object(Config)
 # 3.创建数据库对象
 db = SQLAlchemy(app)
-
-
-
-
+# 4.创建redis数据对象
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, db=Config.REDIS_NUM, decode_responses=True)
+# 5.开启flask后端csrf保护机制，仅仅开启后端csrf验证，
+# 抽取cookie里面的csrf_token和ajax请求头里面的csrf_token进行验证
+# TODO: 设置csrf_token值到cookie中需要自己补充
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def demo():
