73cfff7a2681511c021b87d69501f7d52de92533
sardpade <sunwh1004184@163.com>
2021-06-08 17:24:54 +0800

    抽取配置信息到config.py。创建类，使用类的继承方式，创建接口

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
-# 创建项目管理类
+from redis import StrictRedis
+
+
+
+# 创建项目管理类(父类)
 class Config(object):
 
     DEBUG = True
@@ -17,7 +21,7 @@ class Config(object):
     # 以下是 from flask_session import Session 的配置信息
     # 调整sesion存储到redis配置信息
     # 设置存储session的数据库类型
-    SESSIOM_TYPE = "redis"
+    SESSION_TYPE = "redis"
     # 创建数据库实力对象配置。到时候真正存储数据是到这条数据库，因为使用大写Session改变位置
     SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_NUM)
     # SIGER 签名的意思，在python意思就是需要加密
@@ -32,3 +36,28 @@ class Config(object):
      PERMANENT_SESSION_LIFETIME  这五个值在Session(app)里面，按住ctrl点击进入Session里面看
      ，文档地址：http://pythonhosted.org/Flask-Session/这个地址有解释"""
 
+
+# Config继承父类,这样就什么都有了
+# 创建这个类相当于是开发模式
+class DevelopmenConfig(Config):
+
+    # 开发的时候调试开启debug模式
+    DEBUG = True
+
+
+
+# 创建这个类相当于是线上模式的配置
+class ProductionConifg(object):
+
+    # 上线以后就不用在去开启debug模式了，
+    DEBUG = False
+    # 上线的时候在开启，修改ip地址就可以了
+    # SQLALCHEMY_DATABASE_URI = "mysql://root：mysql@127.0.0.1:3306/new"
+
+
+# 提供一个接口给外界调用
+# 用法：config_dict相当于接口，调用"development"就等于调用--->>>DevelopmenConfig这个类
+config_dict = {
+    "development":DevelopmenConfig,
+    "production":ProductionConifg
+}
--- manager.py
+++ manager.py
@@ -9,60 +9,32 @@ from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
 # 导入管理类，接管app
 from flask_migrate import Manager
+from config import config_dict
 
 
 # 1.创建app对象
 app = Flask(__name__)
 
-# 创建项目管理类
-class Config(object):
-
-    DEBUG = True
-
-    SQLALCHEMY_DATABASE_URI = "mysql://root：mysql@127.0.0.1:3306/new"
-    # 开启数据库跟踪
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    # redis数据库配置信息
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-    # 使用第七个数据库
-    REDIS_NUM = 7
-    # seeion 配置
-    SECRET_KEY = "SLKDJASLKDJKLASLJKDALS"
-
-    # 以下是 from flask_session import Session 的配置信息
-    # 调整sesion存储到redis配置信息
-    # 设置存储session的数据库类型
-    SESSIOM_TYPE = "redis"
-    # 创建数据库实力对象配置。到时候真正存储数据是到这条数据库，因为使用大写Session改变位置
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_NUM)
-    # SIGER 签名的意思，在python意思就是需要加密
-    SESSION_USE_SIGNER = True
-    # SESSION设置有过期时长
-    SESSION_PERMANENT = False
-    # 设置过期时长 默认值：timedelta（days=31）
-    PERMANENT_SESSION_LIFETIME = 86400 * 2
-
-    """SESSIOM_TYPE, SESSION_RREDIS
-     SESSION_USE_SIGNER, SESSION_PERMANENT
-     PERMANENT_SESSION_LIFETIME  这五个值在Session(app)里面，按住ctrl点击进入Session里面看
-     ，文档地址：http://pythonhosted.org/Flask-Session/这个地址有解释"""
-
-
-
 # 2.配置信息注册到APP中
-app.config.from_object(Config)
+# config_dict相当于接口，调用"development"就等于调用--->>>DevelopmenConfig这个类
+config_class = config_dict["development"]
+app.config.from_object(config_class)
+
 # 3.创建数据库对象
 db = SQLAlchemy(app)
+
 # 4.创建redis数据对象
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, db=Config.REDIS_NUM, decode_responses=True)
+redis_store = StrictRedis(host=config_class.REDIS_HOST, port=config_class.REDIS_PORT, db=config_class.REDIS_NUM, decode_responses=True)
+
 # 5.开启flask后端csrf保护机制，仅仅开启后端csrf验证，
 # 抽取cookie里面的csrf_token和ajax请求头里面的csrf_token进行验证
 # TODO: 设置csrf_token值到cookie中需要自己补充
 csrf = CSRFProtect(app)
+
 # 6.# 大写开头的Session ,是借助第三方session类去调整flask中session的存储位置
 # 没有返回值，就这样设置就可以了
 Session(app)
+
 # 7.创建manage管理类
 managr = Manager(app)
 
@@ -75,4 +47,6 @@ def demo():
     return "程序是树，bug是花，没有花哪来的果"
 
 if __name__ == '__main__':
+    # 已经在右上角绿色三角形左边设置了runserver。
+    # 用命令运行的话 python manager.py runserver -h -p -d
     managr.run()
